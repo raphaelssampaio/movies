a381a1cca111018473796433067684b34b92b0cb
Raphael <raphaelssampaio@gmail.com>
2020-07-07 16:27:49 -0300

    Card style

--- src/screens/main.js
+++ src/screens/main.js
@@ -2,6 +2,12 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import { colors } from '../styles'
+import Card from '../components/Card'
+
+import doisMileVinte from '../assets/doisMileVinte.jpg'
+import doisMil from '../assets/doisMil.jpg'
+import doisMileDez from '../assets/doisMileDez.jpg'
+import allTime from '../assets/allTime.jpeg'
 
 export default class Main extends React.Component {
   constructor() {
@@ -10,8 +16,23 @@ export default class Main extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: colors.background,
+          paddingTop: 64,
+          paddingHorizontal: 16,
+        }}
+      >
         <Text style={styles.welcome}>Welcome to Movies</Text>
+        <View style={styles.container}>
+          <Card title="2020 Top 10" picture={doisMileVinte} />
+          <Card title="2010 Top 10" picture={doisMileDez} />
+        </View>
+        <View style={styles.container}>
+          <Card title="2000 Top 10" picture={doisMil} />
+          <Card title="All time top 10" picture={allTime} />
+        </View>
       </View>
     )
   }
@@ -24,6 +45,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: colors.background,
     justifyContent: 'center',
     alignItems: 'center',
