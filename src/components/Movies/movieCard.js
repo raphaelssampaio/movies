5b7a8ad04f8e15d7c3feef0b1f5dde40318bb768
Raphael <raphaelssampaio@gmail.com>
2020-07-15 12:03:00 -0300

    Removing unused fragment

--- src/components/Movies/movieCard.js
+++ src/components/Movies/movieCard.js
@@ -6,50 +6,48 @@ import { goToDetails } from '../../navigation/routes'
 
 export default function MovieCard({ item }) {
   return (
-    <>
-      <View
-        style={{
-          shadowColor: colors.darkBlue,
-          shadowOpacity: 1,
-          elevation: 1,
-          backgroundColor: colors.transparent,
-          marginHorizontal: 5,
-          marginTop: 50,
+    <View
+      style={{
+        shadowColor: colors.darkBlue,
+        shadowOpacity: 1,
+        elevation: 1,
+        backgroundColor: colors.transparent,
+        marginHorizontal: 5,
+        marginTop: 50,
+      }}
+    >
+      <ListItem
+        containerStyle={{
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          backgroundColor: colors.cardBackground,
         }}
-      >
-        <ListItem
-          containerStyle={{
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-            backgroundColor: colors.cardBackground,
-          }}
-          titleStyle={{
-            fontWeight: 'bold',
-            color: colors.white,
-            padding: 15,
-            fontSize: 20,
-          }}
-          subtitleStyle={{
-            color: colors.white,
-            padding: 15,
-            fontSize: 18,
-          }}
-          title={item.name}
-          subtitle={item.subtitle}
-          leftAvatar={{
-            source: { uri: item.avatar_url },
-            size: 150,
-            rounded: false,
-          }}
-        />
-        <Button
-          title={'SEE DETAILS'}
-          buttonStyle={{
-            backgroundColor: colors.darkBlue,
-          }}
-          onPress={() => goToDetails()}
-        />
-      </View>
-    </>
+        titleStyle={{
+          fontWeight: 'bold',
+          color: colors.white,
+          padding: 15,
+          fontSize: 20,
+        }}
+        subtitleStyle={{
+          color: colors.white,
+          padding: 15,
+          fontSize: 18,
+        }}
+        title={item.name}
+        subtitle={item.subtitle}
+        leftAvatar={{
+          source: { uri: item.avatar_url },
+          size: 150,
+          rounded: false,
+        }}
+      />
+      <Button
+        title={'SEE DETAILS'}
+        buttonStyle={{
+          backgroundColor: colors.darkBlue,
+        }}
+        onPress={() => goToDetails()}
+      />
+    </View>
   )
 }
